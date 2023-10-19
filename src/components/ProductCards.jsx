ed2128b3a00fec486ba263e13d592c2ccf099bf9
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-19 17:00:11 +0300

    Build reusable product cards

---
+++ src/components/ProductCards.jsx
@@ -0,0 +1,19 @@
+import ProductCard from "./productCard/ProductCard";
+
+/* eslint-disable react/prop-types */
+export default function ProductCards({ data = [] }) {
+	return (
+		<ul className="grid grid-cols-1 gap-6 w-full">
+			{data.map((item, index) => (
+				<ProductCard
+					key={index}
+					image={item.image}
+					title={item.title}
+					category={item.category}
+					date={item.date}
+					subscribersCount={item.subscribersCount}
+				/>
+			))}
+		</ul>
+	);
+}
