ce872a2c1dd465a11b38c62752f3a1140cd4d945
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-10-19 20:42:10 +0300

    Make Custom Button More Reusable

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
 
-function MainButton({ text = 'Button', handleClick, icon, width = 'auto' }) {
+const btnClasses =
+  'flex gap-2 items-center outline-none transition-all duration-150 ease-in rounded-lg text-md font-semibold px-8 py-3 mr-2';
+
+function MainButton({
+  text = 'Button',
+  handleClick,
+  icon,
+  width = 'auto',
+  className = 'text-white bg-sky-950 hover:bg-teal-200 hover:text-sky-950',
+}) {
+  const customclass = btnClasses + ' ' + className;
+
   return (
     <button
-      className={`flex gap-2 items-center text-white
-    outline-none transition-all duration-150 ease-in
-    rounded-lg text-md font-semibold px-8 py-3 mr-2
-    bg-sky-950 hover:bg-teal-200 hover:text-sky-950`}
+      className={customclass}
       onClick={handleClick}
       style={{ width: width }}
     >
