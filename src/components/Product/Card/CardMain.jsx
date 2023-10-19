67b173fbcfa850c27f69168fede8646574cc07e3
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-19 23:14:16 +0300

    Build car main content component

---
+++ src/components/Product/Card/CardMain.jsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+
+/* eslint-disable react/prop-types */
+export default function CardMain({ title, category }) {
+	return (
+		<>
+			<Typography variant="h6">{title}</Typography>
+			<p
+				className="bg-gradient-to-r from-violet-200 to-teal-300
+            text-sky-950 text-opacity-80 w-24 px-2 py-1.5
+            rounded-md font-semibold mt-1"
+			>
+				{category}
+			</p>
+		</>
+	);
+}
