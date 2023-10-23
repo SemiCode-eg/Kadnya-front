676435cb6d2d773d8f3ffe6274e85b29605579bf
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-23 13:31:12 +0300

    Add isPrimary boolean and type

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -1,32 +1,39 @@
 /* eslint-disable react/prop-types */
 
 const btnClasses =
-  'flex gap-2 items-center outline-none transition-all duration-150 ease-in rounded-lg text-md font-semibold px-8 py-3 mr-2';
+	"flex gap-2 items-center outline-none transition-all duration-150 ease-in rounded-lg text-md font-semibold px-8 py-3 mr-2";
+
+const primaryClass =
+	"text-white bg-sky-950 hover:bg-teal-200 hover:text-sky-950";
 
 function MainButton({
-  text = 'Button',
-  handleClick,
-  icon,
-  className = 'text-white bg-sky-950 hover:bg-teal-200 hover:text-sky-950',
-  reverse = false,
+	text = "Button",
+	handleClick,
+	type = "button",
+	icon,
+	className = "",
+	isPrimary = true,
+	reverse = false,
 }) {
-  const customclass = btnClasses + ' ' + className;
+	const customclass = `${btnClasses} ${
+		isPrimary ? primaryClass : ""
+	} ${className}`;
 
-  return (
-    <button className={customclass} onClick={handleClick}>
-      {reverse ? (
-        <>
-          {text}
-          {icon}
-        </>
-      ) : (
-        <>
-          {icon}
-          {text}
-        </>
-      )}
-    </button>
-  );
+	return (
+		<button type={type} className={customclass} onClick={handleClick}>
+			{reverse ? (
+				<>
+					{text}
+					{icon}
+				</>
+			) : (
+				<>
+					{icon}
+					{text}
+				</>
+			)}
+		</button>
+	);
 }
 
 export default MainButton;
