e324902f488bcde8edfdc176ece39c887a7b5daf
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-23 13:16:37 +0300

    Fix card height

--- src/components/Product/Card/ProductCards.jsx
+++ src/components/Product/Card/ProductCards.jsx
@@ -10,7 +10,7 @@ export default function ProductCards({
 }) {
 	return (
 		<>
-			<ul className="flex flex-col gap-6 pr-5 w-full h-[45dvh] overflow-y-scroll">
+			<ul className="flex flex-col gap-6 pr-5 w-full h-[40dvh] overflow-y-scroll">
 				{data.map((item, index) => (
 					<ProductCard
 						key={index}
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,38 +1,38 @@
-import MiniSide from '../../components/miniSide/MiniSide';
-import AllProducts from './allProducts/AllProducts';
-import Courses from './courses/Courses';
-import { Outlet } from 'react-router-dom';
-import SearchInput from '../../components/SearchInput';
-import CustomCard from '../../components/customCard/CustomCard';
+import MiniSide from "../../components/miniSide/MiniSide";
+import AllProducts from "./allProducts/AllProducts";
+import Courses from "./courses/Courses";
+import { Outlet } from "react-router-dom";
+import SearchInput from "../../components/SearchInput";
+import CustomCard from "../../components/customCard/CustomCard";
 
 const tabs = [
-  {
-    title: 'All Products',
-    path: 'all',
-    content: <AllProducts />,
-  },
-  { title: 'Courses', path: 'courses', content: <Courses /> },
-  { title: 'Coaching', path: 'coaching', content: 'Coaching' },
-  { title: 'Community', path: 'community', content: 'Community' },
-  { title: 'Codcasts', path: 'codcasts', content: 'Podcasts' },
+	{
+		title: "All Products",
+		path: "all",
+		content: <AllProducts />,
+	},
+	{ title: "Courses", path: "courses", content: <Courses /> },
+	{ title: "Coaching", path: "coaching", content: "Coaching" },
+	{ title: "Community", path: "community", content: "Community" },
+	{ title: "Codcasts", path: "codcasts", content: "Podcasts" },
 ];
 
 export default function Products() {
-  const handleSubmit = () => {};
+	const handleSubmit = () => {};
 
-  return (
-    <CustomCard
-      titleComponent={
-        <h1 className="font-bold text-2xl w-full ml-1">Products</h1>
-      }
-    >
-      <SearchInput onSubmit={handleSubmit} />
-      <div className="flex gap-5 mt-5 h-[33rem]">
-        <MiniSide tabs={tabs} />
-        <div className="mainContent w-full h-full overflow-auto">
-          <Outlet />
-        </div>
-      </div>
-    </CustomCard>
-  );
+	return (
+		<CustomCard
+			titleComponent={
+				<h1 className="font-bold text-2xl w-full ml-1">Products</h1>
+			}
+		>
+			<SearchInput onSubmit={handleSubmit} />
+			<div className="flex gap-5 mt-5">
+				<MiniSide tabs={tabs} />
+				<div className="mainContent w-full h-full overflow-auto">
+					<Outlet />
+				</div>
+			</div>
+		</CustomCard>
+	);
 }
