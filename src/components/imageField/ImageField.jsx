700bf24914da57fc62e92bb9750f7b3505125156
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-10-23 14:28:10 +0300

    Finish Image Field

--- src/components/imageField/ImageField.jsx
+++ src/components/imageField/ImageField.jsx
@@ -1,17 +1,50 @@
 /* eslint-disable react/prop-types */
 
-import ImageSquare from '../../assets/icons/ImageSquare.svg';
-import MainButton from '../MainButton/MainButton';
+import { useState } from 'react';
+import ImageSquareColored from '../../assets/icons/ImageSquare.svg';
+import { ImageSquare } from '@phosphor-icons/react';
+import ImageSelectBtn from './ImageSelectBtn';
 
-function ImageField({ isVertical = true }) {
+function ImageField({ isVertical = true, setImageAsset = () => {} }) {
+  const [wrongImageType, setWrongImageType] = useState(false);
+  const [previewedImage, setPreviewedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const { type } = e.target.files[0];
+
+    if (type === 'image/png' || type === 'image/jpeg') {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        setPreviewedImage([reader.result]);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+
+      setWrongImageType(false);
+      setImageAsset(e.target.files[0]);
+    } else {
+      setWrongImageType(true);
+    }
+  };
+
   return isVertical ? (
     <div className="flex flex-col gap-[33px] w-full">
       <div>
-        <div className="rounded-[5px] border-[1px] border-zinc-100 flex justify-center items-center h-auto max-h-[332px] w-full">
-          <button className="bg-white flex justify-center items-center">
-            <ImageSquare size={250} className="text-zinc-300" weight="fill" />
-          </button>
-        </div>
+        {previewedImage ? (
+          <div className="rounded-[5px] border-[1px] flex justify-center items-center h-auto max-h-[332px] w-full overflow-auto">
+            <img
+              src={previewedImage}
+              alt="uploaded-image"
+              className="max-w-full"
+            />
+          </div>
+        ) : (
+          <div className="rounded-[5px] border-[1px] border-zinc-100 flex justify-center items-center h-auto max-h-[332px] w-full">
+            <div className="bg-white flex justify-center items-center">
+              <ImageSquare size={250} className="text-zinc-300" weight="fill" />
+            </div>
+          </div>
+        )}
         <p className="text-[18px] font-[400] text-zinc-400 mt-[5px] text-left">
           Please use .jpg or .png with non-transparent background
         </p>
@@ -20,27 +53,42 @@ function ImageField({ isVertical = true }) {
         <p className="text-[18px] font-[400] text-zinc-400 mb-[5px]">
           Recommended dimensions of 1280x720
         </p>
-        <MainButton
-          text="Select Image"
-          className="text-teal-500 text-[17px] font-[500] border-[1px] border-teal-500 duration-150 hover:text-white hover:bg-teal-500"
-        />
+        <div>
+          <ImageSelectBtn handleChange={handleImageChange} />
+          {wrongImageType && (
+            <p className="text-red-500 text-left mt-[-10px]">
+              Wrong image type
+            </p>
+          )}
+        </div>
       </div>
     </div>
   ) : (
     <div className="flex gap-[19px] w-full">
-      <button className="bg-white rounded-[5px] border-[1px] border-zinc-100 flex justify-center items-center">
-        <img src={ImageSquare} className="w-[98px] opacity-80" />
-      </button>
+      {previewedImage ? (
+        <div className="rounded-[5px] border-[1px] flex justify-center items-center h-auto max-h-[332px] w-full overflow-auto">
+          <img
+            src={previewedImage}
+            alt="uploaded-image"
+            className="max-w-full"
+          />
+        </div>
+      ) : (
+        <div className="rounded-[5px] border-[1px] border-zinc-100 flex justify-center items-center h-auto max-h-[332px] w-full">
+          <div className="bg-white flex justify-center items-center">
+            <img src={ImageSquare} className="w-[98px] opacity-80" />
+          </div>
+        </div>
+      )}
       <div className="text-[15px] font-[400] text-zinc-400 mt-[5px] text-left">
         <p className="mb-[10px]">
           Please use .jpg or .png with non-transparent background
         </p>
         <p className="mb-[10px]">Recommended dimensions of 1280x720</p>
-        <MainButton
-          text="Select Image"
-          isForm={true}
-          className="!px-[14px] text-[15px]"
-        />
+        <ImageSelectBtn handleChange={handleImageChange} />
+        {wrongImageType && (
+          <p className="text-red-500 mt-[10px] text-left">Wrong image type</p>
+        )}
       </div>
     </div>
   );
