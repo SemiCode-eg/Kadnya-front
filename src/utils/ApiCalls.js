43510446f7748be635a46007739cc1d4f8b1302a
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-25 14:55:18 +0300

    Remove console log

--- src/utils/ApiCalls.js
+++ src/utils/ApiCalls.js
@@ -1,62 +1,61 @@
-import api from './ApiUrl';
+import api from "./ApiUrl";
 
 export const getsingleCourse = async (id) => {
-  try {
-    const response = await api.get(`courses/${id}`);
-    return response;
-  } catch (err) {
-    return err;
-  }
+	try {
+		const response = await api.get(`courses/${id}`);
+		return response;
+	} catch (err) {
+		return err;
+	}
 };
 
 export const getCourses = async () => {
-  try {
-    const response = await api.get('courses');
-    console.log(response);
-    return response;
-  } catch (error) {
-    return error;
-  }
+	try {
+		const response = await api.get("courses");
+		return response;
+	} catch (error) {
+		return error;
+	}
 };
 
 export const getModule = async (courseID) => {
-  try {
-    const response = await api.get(`courses/${courseID}/modules`);
+	try {
+		const response = await api.get(`courses/${courseID}/modules`);
 
-    return response;
-  } catch (error) {
-    return error;
-  }
+		return response;
+	} catch (error) {
+		return error;
+	}
 };
 
 export const sendModule = async (moduleData) => {
-  try {
-    const formData = new FormData();
-    formData.append('title', moduleData.title);
-    formData.append('description', moduleData.description);
-    formData.append('course', moduleData.courseID);
-    // formData.append('image', moduleData.imageAsset);
+	try {
+		const formData = new FormData();
+		formData.append("title", moduleData.title);
+		formData.append("description", moduleData.description);
+		formData.append("course", moduleData.courseID);
+		// formData.append('image', moduleData.imageAsset);
 
-    const response = await api.post('modules/create', formData);
+		const response = await api.post("modules/create", formData);
 
-    return response.data;
-  } catch (error) {
-    return error;
-  }
+		return response.data;
+	} catch (error) {
+		return error;
+	}
 };
 
 export const sendSubmodule = async (moduleData) => {
-  try {
-    const formData = new FormData();
-    formData.append('title', moduleData.title);
-    formData.append('description', moduleData.description);
-    formData.append('module', moduleData.module);
-    // formData.append('image', moduleData.imageAsset);
+	try {
+		const formData = new FormData();
+		formData.append("title", moduleData.title);
+		formData.append("description", moduleData.description);
+		formData.append("module", moduleData.module);
+		// formData.append('image', moduleData.imageAsset);
 
-    const response = await api.post('submodules/create', formData);
+		const response = await api.post("submodules/create", formData);
 
-    return response.data;
-  } catch (error) {
-    return error;
-  }
+		return response.data;
+	} catch (error) {
+		return error;
+	}
 };
