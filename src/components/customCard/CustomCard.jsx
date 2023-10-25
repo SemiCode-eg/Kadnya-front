780a501829e939ca5f8e2573d4da26a69e45fd7d
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-10-25 18:43:36 +0300

    Finish MiniSide Responsive

--- src/components/customCard/CustomCard.jsx
+++ src/components/customCard/CustomCard.jsx
@@ -6,7 +6,7 @@ function CustomCard({ children, titleComponent }) {
     <main className="flex flex-col justify-center items-center w-10/12 mx-auto gap-8">
       {titleComponent && titleComponent}
       <Card className="w-full text-center flex justify-center border rounded-md shadow p-5">
-        <CardBody className="flex flex-col gap-3">{children}</CardBody>
+        <CardBody>{children}</CardBody>
       </Card>
     </main>
   );
