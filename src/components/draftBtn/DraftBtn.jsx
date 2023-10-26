08037d6dc6455fe77738131a5d9280d7ecdd3f86
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-10-26 13:06:50 +0300

    Fix import

--- src/components/draftBtn/DraftBtn.jsx
+++ src/components/draftBtn/DraftBtn.jsx
@@ -1,83 +1,83 @@
 /* eslint-disable react/prop-types */
-import { Menu } from '@mui/material';
-import { CaretDown, Eye, EyeSlash } from '@phosphor-icons/react';
-import { useState } from 'react';
-import MenuItems from '../SettingMenu/MenuItems';
+import { Menu } from "@mui/material";
+import { CaretDown, Eye, EyeSlash } from "@phosphor-icons/react";
+import { useState } from "react";
+import MenuItems from "../Menu/MenuItems";
 
 function DraftBtn({ draftState, setDraftState }) {
-  const [title, setTitle] = useState('draft');
-  const [anchorEl, setAnchorEl] = useState(null);
+	const [title, setTitle] = useState("draft");
+	const [anchorEl, setAnchorEl] = useState(null);
 
-  const open = Boolean(anchorEl);
-  const handleClickListItem = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const open = Boolean(anchorEl);
+	const handleClickListItem = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleMenuItemClick = (event) => {
-    switch (event.target.id) {
-      case draftMenuItems[0].text: // draft
-        setTitle('draft');
-        setDraftState(true);
-        break;
-      case draftMenuItems[1].text: // publish
-        setTitle('published');
-        setDraftState(false);
-        break;
-      default:
-        break;
-    }
-    setAnchorEl(null);
-  };
+	const handleMenuItemClick = (event) => {
+		switch (event.target.id) {
+			case draftMenuItems[0].text: // draft
+				setTitle("draft");
+				setDraftState(true);
+				break;
+			case draftMenuItems[1].text: // publish
+				setTitle("published");
+				setDraftState(false);
+				break;
+			default:
+				break;
+		}
+		setAnchorEl(null);
+	};
 
-  return (
-    <>
-      <button
-        className="rounded-[15px] bg-teal-500 text-white font-[500] flex items-center gap-1 py-[4px] px-[9px]"
-        onClick={handleClickListItem}
-      >
-        {draftState ? <EyeSlash weight="bold" /> : <Eye />}
-        <span className="capitalize">{title}</span>
-        <CaretDown />
-      </button>
-      <Menu
-        id="draft-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'draft-menu-button',
-        }}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-        <MenuItems
-          items={draftMenuItems}
-          handlerFunction={handleMenuItemClick}
-        />
-      </Menu>
-    </>
-  );
+	return (
+		<>
+			<button
+				className="rounded-[15px] bg-teal-500 text-white font-[500] flex items-center gap-1 py-[4px] px-[9px]"
+				onClick={handleClickListItem}
+			>
+				{draftState ? <EyeSlash weight="bold" /> : <Eye />}
+				<span className="capitalize">{title}</span>
+				<CaretDown />
+			</button>
+			<Menu
+				id="draft-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "draft-menu-button",
+				}}
+				anchorOrigin={{
+					vertical: "bottom",
+					horizontal: "center",
+				}}
+				transformOrigin={{
+					vertical: "top",
+					horizontal: "center",
+				}}
+			>
+				<MenuItems
+					items={draftMenuItems}
+					handlerFunction={handleMenuItemClick}
+				/>
+			</Menu>
+		</>
+	);
 }
 
 export default DraftBtn;
 
 const draftMenuItems = [
-  {
-    Icon: EyeSlash,
-    text: 'Draft',
-  },
-  {
-    Icon: Eye,
-    text: 'Published',
-  },
+	{
+		Icon: EyeSlash,
+		text: "Draft",
+	},
+	{
+		Icon: Eye,
+		text: "Published",
+	},
 ];
