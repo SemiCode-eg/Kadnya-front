200a0bc234da789dcf30abfe7a2dba632d9e7a39
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-10-27 02:44:24 +0200

    Resolve conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import RootLayout from "./RootLayout.jsx";
-import Products from "./pages/products/Products.jsx";
-import Website from "./pages/website/Website.jsx";
-import Courses from "./pages/products/courses/Courses";
-import AllProducts from "./pages/products/allProducts/AllProducts";
-import SingleCourse from "./pages/course/SingleCourse";
-import Outline from "./pages/course/outline/Outline";
-import EditLesson from "./pages/course/editLesson/EditLesson";
-import CertificateTab from "./pages/course/CertificateTab";
-import SittingTab from "./pages/course/SittingsTab";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import RootLayout from './RootLayout.jsx';
+import Products from './pages/products/Products.jsx';
+import Website from './pages/website/Website.jsx';
+import Courses from './pages/products/courses/Courses';
+import AllProducts from './pages/products/allProducts/AllProducts';
+import SingleCourse from './pages/course/SingleCourse';
+import Outline from './pages/course/outline/Outline';
+import EditLesson from './pages/course/editLesson/EditLesson';
+import CertificateTab from './pages/course/CertificateTab';
+import SittingTab from './pages/course/SittingsTab';
 
 const router = createBrowserRouter([
   {
@@ -30,7 +30,11 @@ const router = createBrowserRouter([
       {
         path: 'products/courses/:id',
         element: <SingleCourse />,
-        children: [{ path: 'outline', element: <Outline /> }],
+        children: [
+          { path: 'outline', element: <Outline /> },
+          { path: 'certificate', element: <CertificateTab /> },
+          { path: 'sittings', element: <SittingTab /> },
+        ],
       },
       {
         path: 'products/courses/:id/edit-lesson/:lissonID',
@@ -40,6 +44,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
