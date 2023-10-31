91f8dd59ede8391226377aba8d357bbcbda43338
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-10-31 22:56:48 +0200

    Fix other routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,93 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import RootLayout from "./RootLayout.jsx";
-import Products from "./pages/products/Products.jsx";
-import Website from "./pages/website/Website.jsx";
-import Courses from "./pages/products/courses/Courses";
-import AllProducts from "./pages/products/allProducts/AllProducts";
-import SingleCourse from "./pages/course/SingleCourse";
-import Outline from "./pages/course/outline/Outline";
-import EditLesson from "./pages/course/editLesson/EditLesson";
-import CertificateTab from "./pages/course/certificateTab/CertificateTab";
-import SittingsTab from "./pages/course/sittingsTab/SittingsTab";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import RootLayout from './RootLayout.jsx';
+import Products from './pages/products/Products.jsx';
+import Website from './pages/website/Website.jsx';
+import Courses from './pages/products/courses/Courses';
+import AllProducts from './pages/products/allProducts/AllProducts';
+import SingleCourse from './pages/course/SingleCourse';
+import Outline from './pages/course/outline/Outline';
+import EditLesson from './pages/course/editLesson/EditLesson';
+import CertificateTab from './pages/course/certificateTab/CertificateTab';
+import SittingsTab from './pages/course/sittingsTab/SittingsTab';
+import Questions from './pages/course/questions/Questions';
+import Results from './pages/course/results/Results';
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <RootLayout />,
-		children: [
-			// { index: true, element: <Products /> }, // Temporary, it will be dashboard component
-			{
-				path: "products",
-				element: <Products />,
-				children: [
-					{ path: "all", element: <AllProducts /> },
-					{ path: "courses", element: <Courses /> },
-					{ path: "coaching", element: <h1>Caoching</h1> },
-				],
-			},
-			{ path: "website", element: <Website /> },
-			{
-				path: "products/courses/:id",
-				element: <SingleCourse />,
-				children: [
-					{ path: "outline", element: <Outline /> },
-					{ path: "certificate", element: <CertificateTab /> },
-					{ path: "sittings", element: <SittingsTab /> },
-				],
-			},
-			{
-				path: "products/courses/:id/edit-lesson/:lessonID",
-				element: <EditLesson />,
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <div className="text-teal-500 font-bold text-3xl flex justify-center">
+            Comming Soon
+          </div>
+        ),
+      }, // Temporary, it will be dashboard component
+      {
+        path: 'products',
+        element: <Products />,
+        children: [
+          { path: 'all', element: <AllProducts /> },
+          { path: 'courses', element: <Courses /> },
+          { path: 'coaching', element: <h1>Caoching</h1> },
+        ],
+      },
+      {
+        path: 'products/courses/:id',
+        element: <SingleCourse />,
+        children: [
+          { path: 'outline', element: <Outline /> },
+          { path: 'certificate', element: <CertificateTab /> },
+          { path: 'sittings', element: <SittingsTab /> },
+          { path: 'questions', element: <Questions /> },
+          { path: 'results', element: <Results /> },
+        ],
+      },
+      {
+        path: 'products/courses/:id/edit-lesson/:lessonID',
+        element: <EditLesson />,
+      },
+      { path: 'website', element: <Website /> },
+      {
+        path: 'sales',
+        element: (
+          <div className="text-teal-500 font-bold text-3xl flex justify-center">
+            Comming Soon
+          </div>
+        ),
+      },
+      {
+        path: 'contacts',
+        element: (
+          <div className="text-teal-500 font-bold text-3xl flex justify-center">
+            Comming Soon
+          </div>
+        ),
+      },
+      {
+        path: 'setting',
+        element: (
+          <div className="text-teal-500 font-bold text-3xl flex justify-center">
+            Comming Soon
+          </div>
+        ),
+      },
+      {
+        path: 'login',
+        element: (
+          <div className="text-teal-500 font-bold text-3xl flex justify-center">
+            Comming Soon
+          </div>
+        ),
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
