1b48f5f389499ecadea82440a1e27846f618041f
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-04 15:53:10 +0200

    Fix responsive issues

--- src/components/customCard/CustomCard.jsx
+++ src/components/customCard/CustomCard.jsx
@@ -5,7 +5,7 @@ function CustomCard({ children, titleComponent }) {
 	return (
 		<main className="flex flex-col justify-center items-center sm:w-11/12 w-11/12 mx-auto gap-8">
 			{titleComponent && titleComponent}
-			<Card className="w-full text-center flex justify-center border rounded-[35px] shadow px-5 py-1">
+			<Card className="w-full text-center flex justify-center border rounded-[35px] shadow sm:px-5 py-1">
 				<CardBody>{children}</CardBody>
 			</Card>
 		</main>
