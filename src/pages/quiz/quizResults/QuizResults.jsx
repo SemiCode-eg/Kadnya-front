602b505562f5bd0ee9d316fb9acfad3cd65d6c1c
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-11-08 16:07:42 +0200

    Init Result Page

--- src/pages/quiz/quizResults/QuizResults.jsx
+++ src/pages/quiz/quizResults/QuizResults.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import SearchInput from '../../../components/SearchInput';
+
 function QuizResults() {
+  const [searchData, setSearchData] = useState(null);
+  const [searchLoading, setSearchLoading] = useState(false);
+
   return (
     <div>
-      Results
+      <SearchInput
+        placeholder="Find results"
+        setData={setSearchData}
+        // URL={`courses/${id}/?q=`}
+        handleLoading={setSearchLoading}
+      />
+      <div className="text-teal-500 font-bold text-3xl flex justify-center mt-5">
+        Comming Soon
+      </div>
     </div>
-  )
+  );
 }
 
-export default QuizResults
+export default QuizResults;
