fae422a11774d5b71f581ebf5ce03fa52ea37ca5
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-20 22:19:30 +0200

    Create addQuestion and getQuiz functions

--- src/utils/ApiCalls.js
+++ src/utils/ApiCalls.js
@@ -209,3 +209,24 @@ export const getCategories = async () => {
 		return error;
 	}
 };
+
+export const addQuestion = async (quizId, data) => {
+	try {
+		const response = await api.post(`mcq_question/create`, {
+			question: data,
+			quiz: quizId,
+		});
+		return response;
+	} catch (error) {
+		return error;
+	}
+};
+
+export const getQuiz = async (id) => {
+	try {
+		const response = await api.get(`quiz/${id}`);
+		return response;
+	} catch (error) {
+		return error;
+	}
+};
