b12ae4ba24518fa1afbc549d9d6102b39f9ea1f3
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-20 22:20:49 +0200

    Initial AddQuiz component

--- src/pages/quiz/addQuiz/AddQuiz.jsx
+++ src/pages/quiz/addQuiz/AddQuiz.jsx
@@ -1,9 +1,21 @@
+import HandleErrorLoad from "../../../components/handleErrorLoad";
+import useQuiz from "../../../hooks/use-quiz";
+import AddedQuestions from "./AddedQuestions";
+import CurrentAddingQuestion from "./CurrentAddingQuestion";
+import { useParams } from "react-router-dom";
+
 function AddQuiz() {
-  return (
-    <div>
-      Add Quiz
-    </div>
-  )
+	const { id } = useParams();
+	const { quizData, loading, errorMsg } = useQuiz(id);
+
+	return (
+		<div className="border-[1.5px] border-[#ddd] rounded-[10px] p-6">
+			<HandleErrorLoad errorMsg={errorMsg} loading={loading}>
+				<AddedQuestions quizData={quizData} />
+				<CurrentAddingQuestion />
+			</HandleErrorLoad>
+		</div>
+	);
 }
 
-export default AddQuiz
+export default AddQuiz;
