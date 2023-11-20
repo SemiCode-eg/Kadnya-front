a0629d9d64221d66a575d7785cb5ddd714f8c08e
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-20 22:18:17 +0200

    Create uss-add-question custom hook

---
+++ src/hooks/use-add-question.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { addQuestion } from "../utils/ApiCalls";
+
+export default function useAddQuestion(quizId, data) {
+	const [loading, setLoading] = useState(false);
+	const [errorMsg, setErrorMsg] = useState("");
+
+	const addNewQuestion = async () => {
+		setLoading(true);
+
+		const res = await addQuestion(quizId, data);
+
+		setLoading(false);
+	};
+
+	return { addNewQuestion, loading, errorMsg };
+}
