4ef900526b573dd54d2d0fb8a926ca35a4e91d3c
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-22 21:45:31 +0200

    Init current adding Question

--- src/pages/quiz/addQuiz/CurrentAddingQuestion.jsx
+++ src/pages/quiz/addQuiz/CurrentAddingQuestion.jsx
@@ -1,3 +1,17 @@
-export default function CurrentAddingQuestion() {
-	return <div></div>;
+import Question from "../../../components/quiz/questions/Question";
+
+export default function CurrentAddingQuestion({
+	questionNum = 1,
+	expanded = "panel1",
+	onClick = () => {},
+	onAddQuestion = () => {},
+}) {
+	return (
+		<Question
+			questionNum={questionNum}
+			expanded={expanded}
+			onClick={onClick}
+			onAddQuestion={onAddQuestion}
+		/>
+	);
 }
