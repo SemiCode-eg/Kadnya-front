fc140da61c68371d5d24a64c5b30c130c063dc36
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-25 09:08:38 +0200

    Make QuestionForm works

--- src/components/quiz/questions/QuestionFrom.jsx
+++ src/components/quiz/questions/QuestionFrom.jsx
@@ -1,18 +1,91 @@
+/* eslint-disable react/prop-types */
 import TextAriaField from "../../forms/TextAriaField";
+import { newQuestionReducerKeys } from "../../../hooks/use-question-reducer";
 import QuestionTypeSelect from "./QuestionTypeSelect";
 import GradSwitch from "./GradSwitch";
-import QuestionOptions from "./QuestionOptions";
 import MainButton from "../../mainButton/MainButton";
+import QuestionChoices from "./QuestionChoices";
 
-export default function QuestionFrom() {
+export default function QuestionFrom({
+	question,
+	onQuestionChange,
+	onAddQuestion,
+	onUpdateQuestion,
+}) {
+	const {
+		question: questionText,
+		questionType,
+		isGraded,
+		choices,
+		trueAnswer,
+	} = question;
+
 	return (
 		<form className="flex flex-col gap-4 items-center">
-			<TextAriaField placeholder="Question" className="w-full" />
-			<QuestionTypeSelect />
-			<GradSwitch />
-			<QuestionOptions />
+			<TextAriaField
+				value={questionText}
+				handleChange={(event) => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.SET_QUESTION,
+						payload: event.target.value,
+					});
+				}}
+				placeholder="Question"
+				className="w-full"
+			/>
 
-			<MainButton text="Add Question" />
+			<QuestionTypeSelect
+				value={questionType}
+				onChange={(value) => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.SET_QUESTION_TYPE,
+						payload: value,
+					});
+				}}
+			/>
+
+			<GradSwitch
+				value={isGraded}
+				onChange={() => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.TOGGLE_IS_GRADED,
+					});
+				}}
+			/>
+
+			<QuestionChoices
+				choices={choices}
+				onAdd={() => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.ADD_CHOICE,
+					});
+				}}
+				onEdit={(index, newValue) => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.EDIT_CHOICE,
+						payload: { index, newValue },
+					});
+				}}
+				trueAnswer={trueAnswer}
+				onTrueAnswerChange={(value) => {
+					onQuestionChange({
+						type: newQuestionReducerKeys.SET_TRUE_ANSWER,
+						payload: value,
+					});
+				}}
+			/>
+
+			<MainButton
+				text={onAddQuestion ? "Add Question" : "Update Question"}
+				className="!m-0"
+				handleClick={() => {
+					if (onAddQuestion) {
+						onAddQuestion(question);
+						return;
+					}
+					onUpdateQuestion(question);
+				}}
+			/>
 		</form>
 	);
 }
