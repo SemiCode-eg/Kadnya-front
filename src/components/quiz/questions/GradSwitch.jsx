c379b379c125feaa7c6a9df032946e65c4b07d27
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-11-25 09:03:07 +0200

    Make auto gradSwitch work

--- src/components/quiz/questions/GradSwitch.jsx
+++ src/components/quiz/questions/GradSwitch.jsx
@@ -1,11 +1,19 @@
+/* eslint-disable react/prop-types */
 import { FormControlLabel, Switch } from "@mui/material";
 
-export default function GradSwitch() {
+export default function GradSwitch({ value, onChange }) {
 	return (
 		<div className="flex w-full">
 			<div className="flex gap-3">
-				<FormControlLabel control={<Switch />} label="Graded question" />
-				<p className="flex justify-center items-center px-4 gap-3 bg-teal-500 rounded-full">
+				<FormControlLabel
+					control={<Switch value={value} onChange={onChange} />}
+					label="Graded question"
+				/>
+				<p
+					className={`flex justify-center items-center px-4 gap-3 rounded-full text-white ${
+						value ? "bg-teal-500" : "bg-gray-400"
+					}`}
+				>
 					<span className="block bg-black rounded-full w-3 h-3"></span>
 					Auto-graded
 				</p>
