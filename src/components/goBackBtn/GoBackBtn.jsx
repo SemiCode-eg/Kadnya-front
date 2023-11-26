fdd34629059c2fc4ffba46e91ee0eadf8255248d
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-11-26 12:57:30 +0200

    Fix back button route

--- src/components/goBackBtn/GoBackBtn.jsx
+++ src/components/goBackBtn/GoBackBtn.jsx
@@ -1,10 +1,11 @@
-import { ArrowLeft } from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { ArrowLeft } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
-function GoBackBtn() {
+function GoBackBtn({ path = "/products/courses" }) {
   return (
     <Link
-      to={'/products/courses'}
+      to={path}
       className="w-[50px] h-[50px] self-start text-white rounded-full flex items-center justify-center duration-100 ease-out bg-gradient-to-r from-violet-300 to-teal-300 hover:from-transparent hover:to-transparent hover:text-sky-950 hover:border-[2px] hover:border-teal-300"
     >
       <ArrowLeft size={35} />
--- src/pages/coaching/Coaching.jsx
+++ src/pages/coaching/Coaching.jsx
@@ -1,21 +1,21 @@
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import CustomCard from '../../components/customCard/CustomCard';
-import GoBackBtn from '../../components/goBackBtn/GoBackBtn';
-import MiniSide from '../../components/miniSide/MiniSide';
-import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import CustomCard from "../../components/customCard/CustomCard";
+import GoBackBtn from "../../components/goBackBtn/GoBackBtn";
+import MiniSide from "../../components/miniSide/MiniSide";
+import { useEffect } from "react";
 
 const tabs = [
   {
-    title: 'Dashboard',
-    path: 'dashboard',
+    title: "Dashboard",
+    path: "dashboard",
   },
   {
-    title: 'Programs',
-    path: 'programs',
+    title: "Programs",
+    path: "programs",
   },
   {
-    title: 'Sittings',
-    path: 'sittings',
+    title: "Sittings",
+    path: "sittings",
   },
 ];
 
@@ -24,8 +24,8 @@ function Coaching() {
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === '/products/coaching') {
-      navigate('dashboard');
+    if (location.pathname === "/products/coaching") {
+      navigate("dashboard");
     }
   }, [navigate, location.pathname]);
 
@@ -34,7 +34,7 @@ function Coaching() {
       <div className="flex lg:flex-row flex-col lg:gap-5 gap-10 mt-5">
         <div className="flex lg:flex-col flex-row lg:justify-normal items-center justify-between gap-[60px]">
           <div className="flex items-center self-start gap-[9px]">
-            <GoBackBtn />
+            <GoBackBtn path="/products/all" />
             <p className="text-sky-950 text-[31px]">Coaching</p>
           </div>
           <MiniSide tabs={tabs} />
