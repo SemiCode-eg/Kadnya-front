cacc0f0b87f6a9f07cdf7f541699d25ebf560cc5
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-12-10 18:32:20 +0200

    Fix logic error

--- src/utils/quiz/validateQuestion.js
+++ src/utils/quiz/validateQuestion.js
@@ -1,19 +1,15 @@
 import { handleQuestionError } from './handleQuestionError'
 import { QUESTION_ERROR_TYPES } from './questionErrorTypes'
 
-export function validateQuestion(
-  questionData,
-  setErrorOpen,
-  dispatchQuestions,
-) {
-  if (questionData.question.trim() === '')
+export function validateQuestion(question, setErrorOpen, dispatchQuestions) {
+  if (question.questionText.trim() === '')
     return handleQuestionError(
       QUESTION_ERROR_TYPES.QUESTION_TEXT,
       'Question field is required',
       dispatchQuestions,
       setErrorOpen,
     )
-  if (!questionData.choices.find(choice => choice === ''))
+  if (!question.choices.find(choice => choice === ''))
     return handleQuestionError(
       QUESTION_ERROR_TYPES.CHOICES_TEXT,
       'Fill the choices',
