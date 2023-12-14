f6be392fd476424777678c8b1fe3c938fbc5370f
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-12-14 19:54:58 +0200

    Create updateQuestion put API function

--- src/api/course/quiz.js
+++ src/api/course/quiz.js
@@ -9,24 +9,37 @@ export const getQuiz = async id => {
   }
 }
 
-export const addQuestion = async (quizId, data) => {
-  try {
-    const response = await api.post(`mcq_question/create`, {
-      question: data,
-      quiz: quizId,
-    })
-    return response
-  } catch (error) {
-    return error
-  }
-}
-
 export const updateQuiz = async (quizData, quizId) => {
   try {
     const response = await api.patch(`quiz/${quizId}/update/`, quizData, {
       headers: { 'content-type': 'multipart/form-data' },
     })
 
+    return response
+  } catch (error) {
+    return error
+  }
+}
+
+export const updateQuestions = async (quizId, questions) => {
+  try {
+    const formData = new FormData()
+
+    questions.forEach(question => {
+      formData.append('question_text', question.questionText)
+      formData.append('question_type', question.questionType)
+      formData.append('is_graded', question.isGraded)
+      formData.append('question_image', question.image)
+      question.choices.forEach((choice, index) => {
+        formData.append(`choices[${index}][choice_text]`, choice.text)
+        formData.append(`choices[${index}][choice_image]`, choice.image)
+        formData.append(`choices[${index}][is_true]`, choice.isTrue)
+      })
+    })
+
+    const response = await api.put(`/quiz_questions/${quizId}/`, formData, {
+      headers: { 'content-type': 'multipart/form-data' },
+    })
     return response
   } catch (error) {
     return error
