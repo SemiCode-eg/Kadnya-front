f6aa72280d469b621942322510f087cb297ff426
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-12-22 11:54:42 +0200

    separate coaching settings into smaller components

--- src/components/coaching/settings/availability/Availability.jsx
+++ src/components/coaching/settings/availability/Availability.jsx
@@ -1,17 +1,10 @@
 import AvailabilityForm from './AvailabilityForm'
+import AvailabilityTitle from './AvailabilityTitle'
 
 function Availability({ data, dispatchSettingsData = () => {} }) {
   return (
     <div className="border-[1.5px] border-[#ddd] rounded-[10px] p-6">
-      <div className="w-full mx-auto mb-8">
-        <p className="text-sky-950 font-[600] text-md tracking-[-0.25px]">
-          Availability
-        </p>
-        <p className="text-neutral-500 text-sm">
-          Select the days and times that you are available to book sessions.
-        </p>
-      </div>
-
+      <AvailabilityTitle />
       <AvailabilityForm
         data={data}
         dispatchSettingsData={dispatchSettingsData}
---
+++ src/components/coaching/settings/noticePeriod/NoticePeriodTitle.jsx
@@ -0,0 +1,14 @@
+function NoticePeriodTitle() {
+  return (
+    <div className="w-full mx-auto mb-8">
+    <p className="text-sky-950 font-[600] text-md tracking-[-0.25px]">
+      Scheduling options
+    </p>
+    <p className="text-neutral-500 text-sm">
+      Select your availability window.
+    </p>
+  </div>
+  )
+}
+
+export default NoticePeriodTitle
