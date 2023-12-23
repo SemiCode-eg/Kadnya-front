ce55fbe2d185c373e59dcc6e3305f7b97c2c7c4a
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-12-23 20:20:01 +0200

    Create getSession function and init updateSession function

--- src/api/coach/dashboard.js
+++ src/api/coach/dashboard.js
@@ -19,3 +19,27 @@ export const getSessionsOverview = async () => {
     return error
   }
 }
+
+export const getSession = async id => {
+  try {
+    const response = await api.get(`session/${id}`)
+
+    return response
+  } catch (error) {
+    return error
+  }
+}
+
+export const updateSession = async (id, session) => {
+  try {
+    const formData = new FormData()
+
+    // TODO append data to the FormData class
+
+    const response = await api.put(`session/${id}update/`, formData)
+
+    return response
+  } catch (error) {
+    return error
+  }
+}
