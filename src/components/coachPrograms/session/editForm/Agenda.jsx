27a51089ece8d13e8b44dc42c22fc830fcae3a34
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-12-24 00:57:27 +0200

    Build agenda list item

--- src/components/coachPrograms/session/editForm/Agenda.jsx
+++ src/components/coachPrograms/session/editForm/Agenda.jsx
@@ -1,3 +1,19 @@
-export default function Agenda() {
-  return <div></div>
+import { IconButton, TextField } from '@mui/material'
+import { Minus } from '@phosphor-icons/react'
+
+export default function Agenda({ index, agenda, editAgenda, deleteAgenda }) {
+  return (
+    <li className="flex gap-2">
+      <TextField
+        variant="standard"
+        placeholder="Agenda"
+        fullWidth
+        value={agenda}
+        onChange={event => editAgenda(index, event.target.value)}
+      />
+      <IconButton onClick={() => deleteAgenda(index)} className="!text-red-500">
+        <Minus />
+      </IconButton>
+    </li>
+  )
 }
