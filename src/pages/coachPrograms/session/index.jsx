f59adba87fe1bda26d396d23f59aac66a32444ad
Salah Mohamed <salah.mohamed0404@gmail.com>
2023-12-24 00:52:19 +0200

    Build Session

--- src/pages/coachPrograms/session/index.jsx
+++ src/pages/coachPrograms/session/index.jsx
@@ -1,14 +1,62 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { deleteSession, updateSession } from '../../../api/coach'
 import { Container, Typography } from '@mui/material'
 import EditForm from '../../../components/coachPrograms/session/editForm'
+import useCoachSession from '../../../hooks/use-coach-session'
+import HandleErrorLoad from '../../../components/handleErrorLoad'
+import useCoachSessionReducer from '../../../hooks/use-coach-session-reducer'
 
 export default function Session() {
-  const { programId, sessionId } = useParams()
+  const { sessionId } = useParams()
+  const { sessionData, errorMsg, loading } = useCoachSession(sessionId)
+  const { session, dispatchSession, sessionKeys } = useCoachSessionReducer()
+  const [sessionLoading, setSessionLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    dispatchSession({ type: sessionKeys.INIT, payload: sessionData })
+  }, [sessionData, dispatchSession, sessionKeys.INIT])
+
+  useEffect(() => {
+    setError(errorMsg)
+  }, [errorMsg])
+
+  const handleSave = async () => {
+    if (session.title.trim() === '')
+      return setError('Please, type session title')
+    if (session.description.trim() === '')
+      return setError('Please, type session description')
+
+    setSessionLoading(true)
+    await updateSession(sessionId, session)
+    setSessionLoading(false)
+  }
+
+  const handleDelete = async () => {
+    setSessionLoading(true)
+    await deleteSession(sessionId)
+    setSessionLoading(false)
+  }
 
   return (
-    <Container>
-      <Typography></Typography>
-      <EditForm />
+    <Container maxWidth="sm">
+      <HandleErrorLoad
+        loading={loading || sessionLoading}
+        errorMsg={error}
+        setErrorMsg={setError}>
+        <Typography variant="h3" component="h1">
+          {sessionData.title}
+        </Typography>
+        <EditForm
+          session={session}
+          dispatchSession={dispatchSession}
+          sessionKeys={sessionKeys}
+          onSave={handleSave}
+          onDelete={handleDelete}
+          setError={setError}
+        />
+      </HandleErrorLoad>
     </Container>
   )
 }
